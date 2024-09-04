2d5a648a88b4d4260c998836cf3bd5ed1373ef1e
[antoine ganem] <[a01644024@tec.mx]>
2024-09-04 10:56:02 -0600

    Endpoints Rutas

--- app/routes/__init_.py
+++ app/routes/__init_.py
@@ -3,6 +3,7 @@ from app.routes.login.login import login_bp
 from app.routes.Indicadores.indicadores import verIndicadores_bp 
 from app.routes.Catalogos.Usuarios import usuarios_bp, usuariosResumen_bp, actUsuario_bp, verUsuarioID_bp
 from app.routes.Catalogos.Empresas import empresas_bp, empresasResumen_bp, actEmpresa_bp, verEmpresaID_bp
+from app.routes.Catalogos.Rutas import rutas_bp, rutasResumen_bp, actRuta_bp
 from app.routes.Filtros.Filtros import verFiltrosCatalogos_bp
 from app.routes.Comercial.Reservas.Reservas import verReservas_bp, nvaReserva_bp
 
@@ -27,6 +28,15 @@ def register_routes(app: Flask):
     app.register_blueprint(empresasResumen_bp)  
     app.register_blueprint(actEmpresa_bp)  
     app.register_blueprint(verEmpresaID_bp)  
+
+
+    ##Catalogo Rutas
+
+    app.register_blueprint(rutas_bp)
+    app.register_blueprint(rutasResumen_bp)
+    app.register_blueprint(actRuta_bp)
+
+    
     
     ##Filtros
     app.register_blueprint(verFiltrosCatalogos_bp)  
